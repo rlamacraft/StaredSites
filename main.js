b53052508343d6162ab100722879c9a87a6292c9
Robert <rlamacraft@gmail.com>
2015-12-28 17:19:44 +0000

    Removed redundant code

--- main.js
+++ main.js
@@ -16,20 +16,6 @@ $(document).ready( function() {
 
 });
 
-
-
-// $('.star').on("mouseleave", function() {
-//   $('.constellation').css("opacity", "0.0");
-// })
-//
-// $('#star1').on("mouseover", function() {
-//   $('.constellation#social').css("opacity", "1.0");
-// } );
-//
-// $('#star3').on("mouseover", function() {
-//   $('.constellation#social').css("opacity", "1.0");
-// } );
-
 $('#background').click(function(evt) {
   addStar("rlamacraft", "http://rlamacraft.co.uk", evt.clientX, evt.clientY);
 })
