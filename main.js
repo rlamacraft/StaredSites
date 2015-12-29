a4a4a2a440540d5161f224c78bca8f8fa7182e1b
Robert <rlamacraft@gmail.com>
2015-12-29 18:30:52 +0000

    Removed sun animation as lowers FPS too much

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ jQuery.getJSON("stars.json", function( stars ) {
 $(document).ready( function() {
 
   setTimeout(function() {
-    $('#picture, #stars, #moon, #sun').css("transform", "translateY(0%)");
+    $('#picture, #moon, #stars').css("transform", "translateY(0%)");
   }, 0);
 
 });
